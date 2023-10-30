f5dfaf8648b5f5f45b93843678e90613d9961845
Maroooooh <mmarahmohamed@gmail.com>
2023-10-30 19:43:09 +0200

    add outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,58 @@
+import "./App.css";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import AppLayout from "./components/appLayout/appLayout";
+import Home from "./pages/home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Explore from "./pages/explore";
+import Lists from "./pages/lists";
+import Join from "./pages/Join";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import NotFound from "./pages/notFound";
+import Profile from "./pages/Profile/profile";
+import Notification from "./pages/Notifications/notification";
+import Mentions from "./pages/Notifications/mentions";
+import Verified from "./pages/Notifications/verified";
+import Communities from "./pages/communities";
+import ProfilePosts from "./pages/Profile/profilePosts";
+import ProfileReplies from "./pages/Profile/profileReplies";
+import ProfileHighlights from "./pages/Profile/profileHighlights";
+import ProfileMedia from "./pages/Profile/profileMedia";
+import ProfileLikes from "./pages/Profile/ProfileLikes";
 
-import './App.css'
-import { createBrowserRouter } from "react-router-dom"
-import { RouterProvider } from "react-router-dom"
-import AppLayout from "./components/appLayout/appLayout"
-import Home from './pages/home'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Explore from './pages/explore';
-import Lists from './pages/lists';
-import NotFound from './pages/notFound';
-import Profile from './pages/profile';
 function App() {
   const router = createBrowserRouter([
     {
-      path:'/',
-      element:<AppLayout/>,
-      children:[
-        {index:true, element: <Home/>},
-        {path: 'explore', element: <Explore/>},
-        {path: 'lists', element: <Lists/>},
-        {path: 'profile', element: <Profile/>},
-
-
-
-        {path: '*', element: <NotFound/>},//wildCard
-        
-
-      ]
-    }
-])
-
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        { index: true, element: <Join /> },
+        { path: "home", element: <Home /> },
+        { path: "explore", element: <Explore /> },
+        { path: "lists", element: <Lists /> },
+        { path: "notifications", element: <Notification /> },
+        { path: "mentions", element: <Mentions /> },
+        { path: "verified", element: <Verified /> },
+        { path: "join", element: <Join /> },
+        { path: "signUp", element: <SignUp /> },
+        { path: "signIn", element: <SignIn /> },
+        {
+          path: "/profile",
+          element: <Profile/>,
+          children: [
+            {index : true , element :<ProfilePosts/>},
+            {path : 'replies' , element:<ProfileReplies/>} ,
+            {path :'highlights',element :<ProfileHighlights/>} ,
+            {path : 'media',element :<ProfileMedia/>} , 
+            {path : 'likes' ,element : <ProfileLikes/>}
+          ],
+        },
+        {path:'communities' , element : <Communities/>},
+        { path: "*", element: <NotFound /> }, //wildCard
+      ],
+    },
+  ]);
 
   return (
     <>
---
+++ src/pages/Profile/ProfileLikes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Post } from "../../components/small/Links";
+
+const ProfileLikes = () => {
+  return (
+    <div>
+      <Post
+        text="اللهُم حقّق لي ما أُريد فأنت تعلم السِر وَ ما يخفي ، اللهُمَ
+                    فوضتك أمري كُله فجمّله خيراً بما شِئت و إجعلني يارب ممن نظرت
+                    إليه فرحمته و سمعت دُعائه فأجبته"
+      />
+    </div>
+  );
+};
+
+export default ProfileLikes;
